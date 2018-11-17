171477cf431fcbe4f4db2d0336cc64a76cd3e218
Maxat Sangerbayev <maxat235@gmail.com>
2018-11-17 22:04:25 +0600

    new bot file

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ import parser
 # In[2]:
 
 
+TOKEN = '737356832:AAEoCl3if1mp_sHijO_Ij_dCyaveZcBcInQ'
 bot = telebot.TeleBot(TOKEN)
 
 
@@ -21,14 +22,14 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['help'])
 def start_handler(message):
-    bot.send_message(message.chat.id, 'To be updated', reply_markup=m.source_markup))
+    bot.send_message(message.chat.id, 'To be updated', reply_markup=m.source_markup)
 bot.polling(none_stop=True)
 
 
 # In[4]:
 
 
-@bot.message_handler(commands=['wts'])
+@bot.message_handler(content_type=['text'])
 def wts_handler(message):
     global isRunning
     if not isRunning:
@@ -38,26 +39,6 @@ def wts_handler(message):
         bot.register_next_step_handler(msg, wtsResponse)
         isRunning = True
 def wtsResponse(message):
-    chat_id = message.chat.id
-    text = message.text.lower()
-    msg = bot.send_message(chat_id, 'Soon I will be able to find it!')
-    isRunning = False
-bot.polling(none_stop=True)
-
-
-# In[5]:
-
-
-@bot.message_handler(commands=['wtb'])
-def wts_handler(message):
-    global isRunning
-    if not isRunning:
-        chat_id = message.chat.id
-        text = message.text
-        msg = bot.send_message(chat_id, 'Enter card name to search')
-        bot.register_next_step_handler(msg, wtbResponse)
-        isRunning = True
-def wtbResponse(message):
     chat_id = message.chat.id
     text = message.text.lower()
     msg = bot.send_message(chat_id, 'Soon I will be able to find it!')
